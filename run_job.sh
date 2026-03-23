c987fba441c6b53c0876c504fd3a1ed7a96d49fc
Cynthia Liu <liucynth@usc.edu>
2026-03-22 20:07:04 -0700

    M0.4 cut over supported launcher (#8)

--- run_job.sh
+++ run_job.sh
@@ -14,4 +14,12 @@ module load cuda/12.4.1
 cd /project2/jieyuz_1727/Continual-Learning
 source venv/bin/activate
 
-python 3B_train.py
+MODEL_ID="/scratch1/ashanmug/models/Llama-3.2-3B-Instruct"
+RUN_ID="slurm_${SLURM_JOB_ID:-manual}"
+
+python main.py \
+  --mode real \
+  --model-name "$MODEL_ID" \
+  --dataset-name temporal_wiki \
+  --run-id "$RUN_ID" \
+  --checkpoint-dir checkpoints
